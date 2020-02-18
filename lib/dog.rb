deb7fac8c3c26f594592896f48f91ae5a4db88e3
Harley McDowell <hmcdowell95@gmail.com>
2020-02-18 19:59:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_accessor :name
+  attr_reader :owner, :mood
+  
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
 end
